b2c28b72a8b99851aaf52c2d9873c6757b627284
Hermes Júnior <hermesj.junior@gmail.com>
2022-05-10 23:14:52 -0300

    Componente SignUp

---
+++ src/components/SignUp/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const $SignUp = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+margin-top: 95px;
+`
